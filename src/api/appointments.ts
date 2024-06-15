3407e3e22887ee52c3d462b4d45369495e18da0b
JC Fermin <ferminejc@gmail.com>
2024-06-15 09:18:17 +0800

    update base url

--- src/api/appointments.ts
+++ src/api/appointments.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://json-server-vercel-beta-ten.vercel.app',
 });
 
 export const fetchAppointments = async () => {
